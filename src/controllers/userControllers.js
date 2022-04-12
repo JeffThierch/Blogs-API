c101497e52328b982f64f39bab7df1ea9681d2b3
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-12 17:41:11 -0300

    :sparkles: Feat: user post route and create user controller

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,17 @@
+const userServices = require('../services/userServices');
+
+const create = async (req, res, next) => {
+  try {
+    const { displayName, email, password, image } = req.body;
+    const token = userServices.create({ displayName, email, password, image });
+
+    return res.status(201).json({ token });
+  } catch (error) {
+    console.error(error);
+    return next(error.message);
+  }
+};
+
+module.exports = {
+  create,
+};
