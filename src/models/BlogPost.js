db25d9ae1bb6d22b0bac6cbd38b38d671848a6a1
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-12 15:50:38 -0300

    :sparkles: Feat: BlogPost Model

---
+++ src/models/BlogPost.js
@@ -0,0 +1,20 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const blogPostModel = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+  }, {
+    underscored: true,
+    timestamps: false,
+    tableName: 'BlogPosts',
+  });
+
+  blogPostModel.associate = (models) => {
+    blogPostModel.belongsTo(models.User, { foreignKey: 'userId', as: 'users' });
+  };
+
+  return blogPostModel;
+};
+
+module.exports = BlogPost;
